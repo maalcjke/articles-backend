726a5bf67dc9319153137a51515101e5b0d238e0
maalcjke <malc.jke@yandex.ru>
2025-08-04 15:43:43 +0300

    feat: typeorm core
    - Added separate config
    - New dependencies installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true, // exclude re-imports into modules
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // exclude re-imports into modules
+    }),
+    TypeOrmModule.forRootAsync(databaseConfig.asProvider()),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,15 @@
+import { registerAs } from '@nestjs/config';
+import * as process from 'node:process';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export default registerAs(
+  'database',
+  (): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: process.env.PG_HOST,
+    port: Number(process.env.PG_PORT ?? 3306),
+    username: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    database: process.env.PG_NAME,
+  }),
+);
